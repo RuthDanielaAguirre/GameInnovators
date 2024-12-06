b61ced9679524802dd8f7845fb62032a9579967a
RuthDanielaAguirre <argdiee@icloud.com>
2024-12-06 09:37:32 +0100

    adding animation

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ fetch("juegos.json")
     const row = document.querySelector(".row");
     data.forEach((element) => {
       const gameCard = `   
-      <div class="card border border-2 border-purple  mx-auto overflow:hidden;" style="width: 15rem;" >
+      <div class="card animate__animated  border border-2 border-purple  mx-auto overflow:hidden;" style="opacity: 0; transition: opacity 0.6s ease-in-out;" data-effect="animate__fadeInUp">
         <img src="${element.imagen}" class="card-img-top mt-3 border  rounded" style="height:12rem; object-fit: cover;" alt="${element.nombre}">
         <div class="card-body d-flex flex-column">
           <h5 class="card-title text-light ">${element.nombre}</h5>
           <p class="card-text text-light">${element.descripcion}</p>
-         <button type="button" class="btn btn-success mt-auto style=""" 
+         <button type="button" class="btn btn-purple btn-purple:hover mt-auto style=""" 
          data-bs-toggle="modal" 
          data-bs-target="#gameModal"
          data-nombre="${element.nombre}"
@@ -31,6 +31,8 @@ fetch("juegos.json")
       </div>`;
       row.innerHTML += gameCard;
     });
+
+    initScrollAnimations();
 
     const modalElement= document.getElementById('gameModal');
     modalElement.addEventListener('show.bs.modal',(event)=>{
@@ -51,7 +53,29 @@ fetch("juegos.json")
       document.getElementById('gameModalCalificacion').textContent =`Calificación: ${calificacion}`;
     });
 
+    
+
   })
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
+
+  function initScrollAnimations(){
+    const cards = document.querySelectorAll('.card');
+
+    const observer = new IntersectionObserver(
+      (entries) =>{
+        entries.forEach((entry)=>{
+          if (entry.isIntersecting){
+            const effect = entry.target.getAttribute("data-effect");
+            entry.target.classList.add(effect);
+            entry.target.computedStyleMap.opacity =1;
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {threshold: 0.1}
+    );
+    cards.forEach((card)=> observer.observe(card));
+  }
+
